cd61651449e073bb25dc4396ce8ad962b966c5d0
Yadi Rosadi <yadi.rosadi@gmail.com>
2016-01-25 17:27:12 +0700

    Update twitter-erl.rb

--- twitter-erl.rb
+++ twitter-erl.rb
@@ -5,17 +5,16 @@ require 'twitter'
 
 
 id = Twitter::REST::Client.new do |config|
-  config.consumer_key = "rT0U8foeDwHSm1LdjFPOY88rf"
-  config.consumer_secret = "oDl6JUhVAWwaQ950M98gFwQ6jG3PIOktBPGBTHcRCnvcIGUeEf"
-  config.access_token = "34250827-7aoPlHhmYfuWSSAZJeDaDj7DAQMftI9GqfFweBkZ3"
-  config.access_token_secret = "mct5jRoUcc2VJBQkA3Ed3X4Hpr50JS7DJLr1TAh2LLdPd"
+  config.consumer_key = "YOUR_CONSUMER_KEY"
+  config.consumer_secret = "YOUR_CONSUMER_SECRET"
+  config.access_token = "YOUR_ACCESS_TOKEN"
+  config.access_token_secret = "YOUR_ACCESS_SECRET"
 end
 
 receive do |f|
   f.when([:post, String]) do |msg|
 
     # connect to twitter & post Hello World
-    #id = Twitter::Base.new('yrsdi', 'f473nC0rp').post(msg).id
     id.update("@yrsdi #{msg}" )
     f.send!([:result, "Tweet sent! Status id: #{msg}"])
 
